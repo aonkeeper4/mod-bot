e20bbca102fa4dac55bc464093d04cd10876ca07
Dylan Strudwick <72671213+n0rmaliz@users.noreply.github.com>
2022-05-17 11:21:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# TODO: add more modules (user management, auto-role, reaction roles, music?, ...)
+
+from discord.ext import commands
+import discord
+import keep_alive
+from utils import send_embed
+
+client = commands.Bot(command_prefix='$')
+
+# load extensions
+client.load_extension("suggestions")
+
+@client.event
+async def on_ready():
+    print('Bot is ready!')
+
+@client.event
+async def on_command_error(ctx, err):
+    if isinstance(err, commands.errors.MissingRequiredArgument):
+        await send_embed(ctx, 'Error', 'Error - missing required argument in command', discord.Colour.red())
+    elif isinstance(err, commands.errors.CommandNotFound):
+        command_name = str(err)[9:-14]
+        await send_embed(ctx, 'Error', f'Error - unrecognised command: {command_name}', discord.Colour.red())
+    else:
+        raise err
+
+keep_alive.keep_alive()
+
+client.run('ODUzOTM5MjI3NzQ2NTAwNjA4.YMcq4Q.Xjzc2uz2gjr3OwwLwlzKsHWHNUA')
---
+++ utils.py
@@ -0,0 +1,21 @@
+from discord.ext import commands
+import discord
+
+async def send_embed(ctx, title, body, colour, r=False, footer='', edit=None): # helper function cus embeds are pain
+    embed = discord.Embed(title=title, description=body, colour=colour)
+    embed.set_footer(text=footer)
+    if edit is None:
+        msg = await ctx.send(embed=embed)
+    else:
+        msg = edit
+        await edit.edit(embed=embed)
+    return msg if r else None
+
+async def wait_for_message(ctx, check, timeout=60): # another helper function weeee
+    try:
+        message = await client.wait_for('message', timeout=timeout, check=check)
+    except asyncio.TimeoutError:
+        await send_embed(ctx, 'Error', 'Error')
+        return
+    else:
+        return message
